1b5a4296b504c5a0380699e916354ff4021a7628
aliaksandra.lipai <aliaksandra.lipai@altoros.com>
2019-02-19 10:52:33 +0300

    change description

--- resources/views/ppc_altoroscom/index.blade.php
+++ resources/views/ppc_altoroscom/index.blade.php
@@ -19,7 +19,7 @@
 <div id="app">
     <span class="center">
         This web application is designed to collect statistics from Google Adwords for an arbitrary date range.
-        Select which report to enter data and date range, then click Processing.
+        Select date range, then click Processing.
     </span>
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container center">
